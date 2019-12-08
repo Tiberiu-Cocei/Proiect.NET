29b969ed603a1743ceaf76809dbb1c4536c446b6
Marcel1123 <48123341+Marcel1123@users.noreply.github.com>
2019-12-08 12:49:55 +0200

    Created services and repository for all eltity

---
+++ SmartCity.DataAccess/Repositories/Bus/IBusRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartCity.DataAccess.Repositories.Bus
+{
+    class IBusRepository
+    {
+    }
+}
---
+++ SmartCity.DataAccess/Repositories/BusRoute/IBusRouteRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartCity.DataAccess.Repositories.BusRoute
+{
+    class IBusRouteRepository
+    {
+    }
+}
