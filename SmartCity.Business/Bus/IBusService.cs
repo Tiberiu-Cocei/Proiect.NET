d0d74d7cf87ac60f7cae295abce2efad5247495f
Andrei2498 <48515205+Andrei2498@users.noreply.github.com>
2019-12-08 13:24:32 +0200

    small fix x2

--- SmartCity.Business/Bus/IBusService.cs
+++ SmartCity.Business/Bus/IBusService.cs
@@ -1,10 +1,21 @@
-﻿using System;
+﻿using SmartCity.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace SmartCity.Business.Bus
 {
     public interface IBusService
     {
+        Task AddAsync(BusEntity entity);
+
+        Task<ICollection<BusEntity>> GetAsync();
+
+        Task<BusEntity> GetByIdAsync(Guid id);
+
+        Task DeleteAsync(Guid id);
+
+        Task UpdateAsync(BusEntity bus);
     }
 }
