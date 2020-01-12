32cd3573839d42f8c6012a6a906cacea44013e68
Cocei Tiberiu <tiberiu.cocei@gmail.com>
2020-01-12 21:29:56 +0200

    Password Hashing extension method

---
+++ SmartCity.Domain/ExtensionMethods/PersonExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+namespace SmartCity.Domain.ExtensionMethods
+{
+    public static class PersonExtensions
+    {
+        public static string PasswordHashing(string password)
+        {
+            var bytes = new UTF8Encoding().GetBytes(password);
+            byte[] hashBytes;
+            using (var algorithm = new System.Security.Cryptography.SHA512Managed())
+            {
+                hashBytes = algorithm.ComputeHash(bytes);
+            }
+            return Convert.ToBase64String(hashBytes);
+        }
+    }
+}
---
+++ SmartCity.Tests/Domain/PersonExtensionsTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SmartCity.Domain.ExtensionMethods;
+
+namespace SmartCity.Tests.Domain
+{
+    [TestClass]
+    public class PersonExtensionsTest
+    {
+        [TestMethod]
+        public void WhenPasswordHashingIsCalledWithSamePasswords_ThenResultIsIdentical()
+        {
+            // Arrange
+            string passwordToTest = "password1234";
+
+            // Act
+            string hashedPasswordOne = PersonExtensions.PasswordHashing(passwordToTest);
+            string hashedPasswordTwo = PersonExtensions.PasswordHashing(passwordToTest);
+
+            // Assert
+            Assert.AreEqual(hashedPasswordOne, hashedPasswordTwo);
+        }
+
+        [TestMethod]
+        public void WhenPasswordHashingIsCalledWithDifferentPasswords_ThenResultIsDifferent()
+        {
+            // Arrange
+            string passwordToTest = "password1234";
+            string passwordToTestTwo = "password123456";
+
+            // Act
+            string hashedPasswordOne = PersonExtensions.PasswordHashing(passwordToTest);
+            string hashedPasswordTwo = PersonExtensions.PasswordHashing(passwordToTestTwo);
+
+            // Assert
+            Assert.AreNotEqual(hashedPasswordOne, hashedPasswordTwo);
+        }
+    }
+}
